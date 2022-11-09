ff137e7e0ddb1d6dab5c4d9a5a1494e8c374eae3
Jansen <jansenmtan@gmail.com>
2022-11-09 15:21:00 -0600

    implement basic login view. functionality not added. model and tests need to be implemented.

--- airline/main/urls.py
+++ airline/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
         path('', views.IndexView.as_view(), name='index'),
+        path('login/', auth_views.LoginView.as_view(template_name='main/login.html')),
         ]
